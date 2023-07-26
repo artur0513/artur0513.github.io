7f9b3a06d13e9416dad6b438a1b7bf4e5e251034
artur0513 <artur.salaridze@yandex.ru>
2023-07-26 13:14:39 +0300

    fetch test

--- script1.js
+++ script1.js
@@ -1,4 +1,14 @@
 var c = document.getElementById('c1');
 var gl = c.getContext('experimental-webgl');
+
+async function loadData(){
+let url = 'https://raw.githubusercontent.com/artur0513/artur0513.github.io/main/fetch_test.txt';
+let response = await fetch(url);
+let text = await response.text(); // прочитать тело ответа как текст
+alert(text.slice(0, 80) + '...');
+}
+
+loadData();
+
 gl.clearColor(0,0,0.8,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
